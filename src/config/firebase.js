1264610ef908acc0086c2270784771bb27ab9602
marileidy2829 <marileidycastillo029@gmail.com>
2021-04-28 15:30:20 -0400

    instalacion del npm , limpieza de app.js y creacion de la carpeta config y firebase/app

---
+++ src/config/firebase.js
@@ -0,0 +1,12 @@
+// firebase deps
+import firebase from 'firebase/app';
+import 'firebase/auth' ;
+import 'firebase/firebase' ;
+// incializar firebase
+
+firebase.initializeApp ({
+    /*
+    esta infirmacion te la entregar en tu proyeccto firebase y es 
+    distinta en cada proyecto.
+    */
+});
