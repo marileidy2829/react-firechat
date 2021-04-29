c2576a1d120eb96994b11c3d28e5203e873dee99
marileidy2829 <marileidycastillo029@gmail.com>
2021-04-29 00:35:46 -0400

    } from './config/firebase';

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,9 +4,34 @@ import 'firebase/auth' ;
 import 'firebase/firebase' ;
 // incializar firebase
 
-firebase.initializeApp ({
-    /*
-    esta infirmacion te la entregar en tu proyeccto firebase y es 
-    distinta en cada proyecto.
-    */
-});
+const firebaseConfig = {
+    apiKey: "AIzaSyAwUNbJGdxcN0zz5YYqYKeT1kM-E63TBDU",
+    authDomain: "firechat-4b15f.firebaseapp.com",
+    databaseURL: "https://firechat-4b15f-default-rtdb.firebaseio.com",
+    projectId: "firechat-4b15f",
+    storageBucket: "firechat-4b15f.appspot.com",
+    messagingSenderId: "200578898619",
+    appId: "1:200578898619:web:1bf62ee99f939621ff279e",
+    measurementId: "G-4YFT28DBY4"
+  };
+
+firebase.initializeApp (firebaseConfig);
+//tomo los elementos basicos para trabajar con firebase
+
+const db = firebase.firestore();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export {
+    db,
+    googleAuthProvider,
+    firebase
+} from './config/firebase';
+
+function app (){
+    return (
+        <div></div>
+    );
+}
+
+export default app;
+  
