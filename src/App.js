74ff0fe687d5304f20bfcd5450a2cd2158e4e321
arlinj <arlindejesusmartinez919@gmail.com>
2021-05-07 21:38:15 -1200

    Modificaciones en app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import { googleAuthProvider, firebase} from './config/firebase';
+import { signInWithGoogle, signOut} from './login';
 import Button from './components/Button';
 
 function App(){
-  const signInWithGoogle = async () =>{
-    firebase.auth().useDeviceLanguage();
-    try {
-      await firebase.auth().signInWithPopup(googleAuthProvider);
-    }catch (e){
-      console.error(e.message);
-    }
-  };
-
+  
 return (
   <div>
-    <Button onClick ={signInWithGoogle}> sing in with Google</Button>
+
+    {
+      user ? (
+        <>
+        <Button onClick={signOut} > Sing in with Google</Button>
+      <p> Bienvenidos al CHAT!</p>
+      </>
+        ) : <Button onClick={signInWithGoogle} > Sing in with  Google </Button>
+    }
   </div>
 );
-};
+  }  
 export default App;
-
 
