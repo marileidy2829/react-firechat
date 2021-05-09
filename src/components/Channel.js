ab8e7939bf9533f2ff65db424784ca730245dca2
Anabel Henriquez <anabelperez026@gmail.com>
2021-05-08 20:43:21 -0500

    Add query for masseges from firestor

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,6 +1,31 @@
+
+
+import { userEffect, useState} from 'react';
+import { bd } from '../config/firebase';
+
 const Channel = ({ user = null}) => {
-    const [messages, setMesages] = useState([]),
-    return <ul></ul>
+const [messages, setMessages] = useState([]),
+//Necesito cargar los mensage de la BD.
+userEffect(() => {
+/*
+Crear el query de los mensajes.
+cargar 100 mensages ordenados por fechas.
+*/
+const query = bd.collection('messages')
+      .orderBy('createdAt')
+      .limit(100);
+const unsubscribe = query.onSnapshot(querySnapshot =>{
+    //Obtiene todos los mensages desde la bd con su Id.
+    const date = query.onSnapshot.docs.map(doc => ({
+        ...doc.data(),
+        id: doc.id,
+    }));
+    //Actualizo todos los mensages octenido desde la bd.
+    setMessages(data);
+});
+},[])
+
+return <ul></ul>
 
 };
 export default Channel;
