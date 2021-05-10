d8ac327dac28fd38dc337d6ae17b545a19be63bb
Anabel Henriquez <anabelperez026@gmail.com>
2021-05-10 10:54:08 -0500

    Fix mergen conflit between channel-anabel

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -8,8 +8,7 @@ const Channel = ({ user = null }) => {
        
 //Necesito cargar los mensage de la BD.
 useEffect
-(() => {;;
-    
+(() => {
 /*
 Crear el query de los mensajes.
 cargar 100 mensages ordenados por fechas.
